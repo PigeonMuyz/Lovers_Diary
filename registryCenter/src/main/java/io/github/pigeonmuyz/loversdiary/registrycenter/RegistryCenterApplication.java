ddc1b30cef8f5828208a7744448fd7455394fbe5
Tianchen <muyz0319@icloud.com>
2022-04-21 00:38:47 +0800

    Gateway Commit

---
+++ registryCenter/src/main/java/io/github/pigeonmuyz/loversdiary/registrycenter/RegistryCenterApplication.java
@@ -0,0 +1,13 @@
+package io.github.pigeonmuyz.loversdiary.registrycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryCenterApplication.class, args);
+    }
+
+}
---
+++ registryCenter/src/test/java/io/github/pigeonmuyz/loversdiary/registrycenter/RegistryCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pigeonmuyz.loversdiary.registrycenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

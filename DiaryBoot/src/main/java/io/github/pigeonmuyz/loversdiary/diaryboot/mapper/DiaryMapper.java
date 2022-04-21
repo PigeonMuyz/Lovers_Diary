38c8f755859afdf028604b35f5499c4d07822f18
Tianchen <muyz0319@icloud.com>
2022-04-21 21:26:38 +0800

    Mapper Commit

---
+++ DiaryBoot/src/main/java/io/github/pigeonmuyz/loversdiary/diaryboot/mapper/DiaryMapper.java
@@ -0,0 +1,7 @@
+package io.github.pigeonmuyz.loversdiary.diaryboot.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import io.github.pigeonmuyz.loversdiary.utils.Diary;
+
+public interface DiaryMapper extends Mapper<Diary> {
+}

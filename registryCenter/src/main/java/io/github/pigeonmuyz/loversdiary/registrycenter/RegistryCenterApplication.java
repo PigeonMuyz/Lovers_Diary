55225f1a7b8bbf792ea26b733cf644d868a58f4d
Tianchen <muyz0319@icloud.com>
2022-04-21 09:39:50 +0800

    Update Registry

--- registryCenter/src/main/java/io/github/pigeonmuyz/loversdiary/registrycenter/RegistryCenterApplication.java
+++ registryCenter/src/main/java/io/github/pigeonmuyz/loversdiary/registrycenter/RegistryCenterApplication.java
@@ -2,7 +2,9 @@ package io.github.pigeonmuyz.loversdiary.registrycenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class RegistryCenterApplication {
 

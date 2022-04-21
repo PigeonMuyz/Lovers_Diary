46aea2dd3d4745a91b74e50e310b02bbef616204
Tianchen <muyz0319@icloud.com>
2022-04-21 09:40:13 +0800

    Default Commit

--- Gateway/src/main/java/io/github/pigeonmuyz/loversdiary/gateway/GatewayApplication.java
+++ Gateway/src/main/java/io/github/pigeonmuyz/loversdiary/gateway/GatewayApplication.java
@@ -2,7 +2,9 @@ package io.github.pigeonmuyz.loversdiary.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class GatewayApplication {
 
